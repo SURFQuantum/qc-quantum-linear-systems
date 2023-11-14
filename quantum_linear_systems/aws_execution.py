09146294ebb22aae5ed1d50c890a4441c3f4773e
dmark04 <dmark04@web.de>
2023-11-14 09:55:52 +0100

    aws real arg

--- quantum_linear_systems/aws_execution.py
+++ quantum_linear_systems/aws_execution.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import time
@@ -102,6 +103,14 @@ def aws_s3_folder(folder_name: str) -> Tuple[str, str]:
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="AWS run script for a hybrid job on a simulator or a real quantum device."
+    )
+    parser.add_argument(
+        "--real", action="store_true", help="Use a real device instead of a simulator"
+    )
+    args = parser.parse_args()
+
     # SURF-ResearchCloud setup
     my_prefix = "quantum_linear_systems"
     s3_folder = aws_s3_folder(my_prefix)
@@ -109,11 +118,13 @@ if __name__ == "__main__":
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
     # get account
     aws_account_id = boto3.client("sts").get_caller_identity()["Account"]
-
-    device_arn = Devices.Amazon.SV1
+    # set device
+    if args.real:
+        device_arn = "arn:aws:braket:eu-west-2::device/qpu/oqc/Lucy"
+    else:
+        device_arn = Devices.Amazon.SV1
     device = AwsDevice(device_arn)
 
-    # set device
     # Create a client for the IAM service
     iam_client = boto3.client("iam")
     # check  roles
